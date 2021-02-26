48db1735af1dcc0a16aef991c853e4f062607323
Thigo <thiago.l17@yahoo.com>
2021-02-25 23:16:18 -0300

    adding surveys class

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveysRepository } from './repositories/SurveysRepository';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysRepository();
 
 router.post('/users', userController.create);
+router.post('/surveys', surveysController.create);
 
-  export { router };
+export { router };
